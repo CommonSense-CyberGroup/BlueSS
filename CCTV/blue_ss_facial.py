521989ab76140c5a1d7e26d7235554b11ca68495
CommonSense-CyberGroup <92452576+CommonSense-CyberGroup@users.noreply.github.com>
2021-11-20 21:50:08 -0700

    Create file

---
+++ CCTV/blue_ss_facial.py
@@ -0,0 +1,39 @@
+"""
+TITLE: BlueSS Facial
+BY:
+    Some Guy they call Scooter
+    Common Sense Cyber Group
+
+Created: 11/20/2021
+Updated: 11/20/2021
+
+Version: 1.0.1
+
+License: MIT
+
+Purpose:
+    -The purpose of this script is to run facial and human recognition on alerts and recordings from the BlueIris CCTV System
+    -Upon seeing a face (positive ID or unknown) or a human body, an alert will be sent to the configured list of recipients via Email
+    -This script was built intended to be used in Windoes, buut is written in such a way that it should work on most platforms
+
+Considerations:
+    -The Haar Cascade and LBP models are used in this script for recognition. We use DLIB in order to create our dataset to run a face againse
+        *DLIB IS OUTSIDE THE SCOPE OF THIS SCRIPT AND WE USE A PRE-PROCESSED FILE FOR EVERYTHING AS THIS CAN TAKE A LONG TIME*
+
+To Do:
+    -
+
+"""
+
+### IMPORT LIBRARIES ###
+
+
+### DEFINE VARIABLES ###
+
+
+### CLASSES AND FUNCTIONS ###
+
+
+### THE THING ###
+if __name__ == '__main__':
+    print()
